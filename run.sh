d09eeeff17355833e8cc2be35d70975710f4d8a6
Ayuto Yamada <a-yamada@matlab.nitech.ac.jp>
2024-06-20 17:28:54 +0900

    Modify run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <builddir>"
+    exit 1
+fi
+
 pn=100000000
 
-./build/nf --pktnum=$pn &
+$1/nf --pktnum=$pn &
 sleep 1
-./build/rx --pktnum=$pn &
+$1/rx --pktnum=$pn &
 sleep 1
-./build/tx --pktnum=$pn
+$1/tx --pktnum=$pn
