2762cad35548fa742472fe92998676d91fa25163
Ayuto Yamada <a-yamada@matlab.nitech.ac.jp>
2024-06-20 11:06:05 +0900

    Introduce macros about metadata to change metadata size

--- lib/mbuf/common/mbuf_core.h
+++ lib/mbuf/common/mbuf_core.h
@@ -8,9 +8,14 @@
 #include <pktbuf_get_put.h>
 
 #define BUF_NUM 163456
-#define METADATA_SIZE 128
+#define METADATA_TOTAL_SIZE 128
+#define METADATA_RESERV_SIZE 48
 #define MBUF_HEADROOM_SIZE 128
 #define DATAROOM_SIZE (MBUF_HEADROOM_SIZE + 2048)
+
+#ifndef METADATA_SIZE
+#define METADATA_SIZE METADATA_TOTAL_SIZE
+#endif
 
 struct metadata {
     uint32_t pkt_len;
@@ -22,8 +27,15 @@ struct metadata {
 static inline void
 reset_metadata(struct metadata *md)
 {
-    memset(md, 0, CACHE_LINE_SIZE + 8 + 8);
+#if METADATA_SIZE >= METADATA_TOTAL_SIZE - METADATA_RESERV_SIZE
+    memset(md, 0, METADATA_TOTAL_SIZE - METADATA_RESERV_SIZE);
+#else
+    memset(md, 0, METADATA_SIZE);
+#endif
+
+#if METADATA_SIZE >= 8
     md->nb_segs = 1;
+#endif
 }
 
 #endif /* _MBUF_CORE_H_ */
