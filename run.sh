83760c54d8191001024f98289e66b535f8a16e40
Ayuto Yamada <a-yamada@matlab.nitech.ac.jp>
2024-07-19 16:07:27 +0900

    run.sh requires vqsz as an arg

--- run.sh
+++ run.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <builddir> <batchsz>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <builddir> <batchsz> <vqsz>"
     exit 1
 fi
 
-pn=102400000
+pn=100000000
 
-if [ $2 -lt 128 ]; then
-    vqsz=256
-else
-    vqsz=$(expr 2 \* $2)
-fi
+batchsz=$2
+vqsz=$3
+mobjcache=$vqsz
 
-echo "pktnum=${pn}"
-echo "batchsz=$2"
+echo "batchsz=$batchsz"
 echo "vqsz=$vqsz"
+echo "mobjcache=${mobjcache}"
 
-sudo $1/nf --pktnum=$pn -H --batchsz=$2 --vqsz=$vqsz --mobjcache=$vqsz &
+sudo $1/nf --pktnum=$pn -H --batchsz=$2 --vqsz=$vqsz --mobjcache=$mobjcache &
 sleep 3
-sudo $1/rx --pktnum=$pn -H --batchsz=$2 --vqsz=$vqsz --mobjcache=$vqsz &
+sudo $1/rx --pktnum=$pn -H --batchsz=$2 --vqsz=$vqsz --mobjcache=$mobjcache &
 sleep 3
-sudo $1/tx --pktnum=$pn -H --batchsz=$2 --vqsz=$vqsz --mobjcache=$vqsz
+sudo $1/tx --pktnum=$pn -H --batchsz=$2 --vqsz=$vqsz --mobjcache=$mobjcache
 
